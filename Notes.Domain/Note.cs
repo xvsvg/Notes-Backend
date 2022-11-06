fad3b2c09a9bc5f6fc34475e11395f125b3e320d
xvsvg <pobedotriples@gmail.com>
2022-11-06 05:09:45 +0300

    feat: add Note entity

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Notes.Domain;
+
+public class Note
+{
+    public Guid UserId { get; private set; }
+    public Guid NoteId { get; private set; }
+    public string NoteName { get; private set; }
+    public string Details { get; private set; }
+    public DateTime CreationDate { get; private set; }
+    public DateTime? EditionDate { get; private set; }
+
+}
